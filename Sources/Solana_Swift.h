0eb2da9c3dd3038e25fa5a48d02a92dba783d62a
Andrew Son <andoran90@gmail.com>
2022-10-20 18:54:15 +0500

    Add project to framework Update gitignore Add cocoapods

---
+++ Sources/Solana_Swift.h
@@ -0,0 +1,18 @@
+//
+//  Solana_Swift.h
+//  Solana.Swift
+//
+//  Created by Andrew Son on 20/10/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Solana_Swift.
+FOUNDATION_EXPORT double Solana_SwiftVersionNumber;
+
+//! Project version string for Solana_Swift.
+FOUNDATION_EXPORT const unsigned char Solana_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Solana_Swift/PublicHeader.h>
+
+
