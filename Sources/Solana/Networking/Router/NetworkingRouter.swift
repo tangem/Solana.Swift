c3acb6f97452a9a6850f9688b3927f2599f4424d
Andrew Son <andoran90@gmail.com>
2022-10-21 14:57:49 +0500

    Update counter reset logic

--- Sources/Solana/Networking/Router/NetworkingRouter.swift
+++ Sources/Solana/Networking/Router/NetworkingRouter.swift
@@ -23,12 +23,15 @@ public class NetworkingRouter: SolanaRouter {
         endpoints[currentEndpointIndex]
     }
     
+    private var host: String? {
+        endpoint.url.host
+    }
+    
     private let urlSession: URLSession
     private let endpoints: [RPCEndpoint]
     
     private var currentEndpointIndex = 0
     private var bag = Set<AnyCancellable>()
-    private var firedRequestsCounters: [String: Int] = [:]
     
     public init(endpoints: [RPCEndpoint], session: URLSession = .shared) {
         self.endpoints = endpoints
@@ -42,9 +45,7 @@ public class NetworkingRouter: SolanaRouter {
         onComplete: @escaping (Result<T, Error>) -> Void
     ) {
         let bcMethod = bcMethod.replacingOccurrences(of: "\\([\\w\\s:]*\\)", with: "", options: .regularExpression)
-        var endpointIndex = firedRequestsCounters[bcMethod] ?? 0
-        endpointIndex = endpointIndex >= endpoints.count ? 0 : endpointIndex
-        let url = endpoints[endpointIndex].url
+        let url = endpoint.url
         let params = parameters.compactMap {$0}
         let requestAPI = SolanaRequest(method: bcMethod, params: params)
         
@@ -58,76 +59,80 @@ public class NetworkingRouter: SolanaRouter {
             onComplete(.failure(error))
         }
         
-        bag.insert(
-            urlSession.dataTaskPublisher(for: request)
-                .tryMap { (data: Data?, response: URLResponse) -> Void in
-                    guard let httpURLResponse = response as? HTTPURLResponse else {
-                        throw RPCError.httpError
-                    }
-                    
-                    switch httpURLResponse.statusCode {
-                    case 200..<300:
-                        break
-                    case 429:
-                        throw RPCError.retry
-                    default:
-                        throw RPCError.httpErrorCode(httpURLResponse.statusCode)
-                    }
-                    
-                    guard let data = data else {
-                        throw RPCError.invalidResponseNoData
-                    }
-                    
-                    let decodedResponse = try JSONDecoder().decode(Response<T>.self, from: data)
-                    
-                    if let result = decodedResponse.result {
-                        onComplete(.success(result))
-                        return
-                    }
-                    if let responseError = decodedResponse.error {
-                        throw RPCError.invalidResponse(responseError)
-                    } else {
-                        throw RPCError.unknownResponse
-                    }
+        urlSession.dataTaskPublisher(for: request)
+            .tryMap { (data: Data?, response: URLResponse) -> Void in
+                guard let httpURLResponse = response as? HTTPURLResponse else {
+                    throw RPCError.httpError
                 }
-                .retry(2)
-                .sink(receiveCompletion: { [weak self] completion in
-                    guard case let .failure(error) = completion else {
-                        return
-                    }
-                    
-                    func retry() {
-                        self?.request(method: method,
-                                      bcMethod: bcMethod,
-                                      parameters: parameters,
-                                      onComplete: onComplete)
-                    }
-                    
-                    if let solanaError = error as? RPCError,
-                       case .retry = solanaError {
-                        retry()
-                        return
-                    }
-                    guard self?.needRetry(for: bcMethod) ?? false else {
-                        print("Failed to send request: \(bcMethod) \(url)")
-                        onComplete(.failure(error))
-                        return
-                    }
-                    
+                
+                switch httpURLResponse.statusCode {
+                case 200..<300:
+                    break
+                case 429:
+                    throw RPCError.retry
+                default:
+                    throw RPCError.httpErrorCode(httpURLResponse.statusCode)
+                }
+                
+                guard let data = data else {
+                    throw RPCError.invalidResponseNoData
+                }
+                
+                let decodedResponse = try JSONDecoder().decode(Response<T>.self, from: data)
+                
+                if let result = decodedResponse.result {
+                    onComplete(.success(result))
+                    return
+                }
+                if let responseError = decodedResponse.error {
+                    throw RPCError.invalidResponse(responseError)
+                } else {
+                    throw RPCError.unknownResponse
+                }
+            }
+            .retry(2)
+            .sink(receiveCompletion: { [weak self] completion in
+                guard let self = self else { return }
+                
+                guard case let .failure(error) = completion else {
+                    return
+                }
+                
+                func retry() {
+                    self.request(method: method,
+                                  bcMethod: bcMethod,
+                                  parameters: parameters,
+                                  onComplete: onComplete)
+                }
+                
+                if let solanaError = error as? RPCError,
+                   case .retry = solanaError {
                     retry()
-                }, receiveValue: { })
-        )
+                    return
+                }
+                
+                guard self.needRetry(for: url.host) else {
+                    print("Failed to send request: \(bcMethod) \(url)")
+                    onComplete(.failure(error))
+                    return
+                }
+                
+                retry()
+            }, receiveValue: { })
+            .store(in: &bag)
     }
     
-    private func needRetry(for function: String) -> Bool {
-        let name = function
-        let currentEndpointIndex = (firedRequestsCounters[name] ?? 0) + 1
-        firedRequestsCounters[name] = currentEndpointIndex
+    private func needRetry(for errorHost: String?) -> Bool {
+        if self.host != errorHost {
+            return true
+        }
+        
+        currentEndpointIndex += 1
         if currentEndpointIndex < endpoints.count {
             return true
         }
         
-        firedRequestsCounters[name] = 0
+        currentEndpointIndex = 0
         return false
     }
 }
