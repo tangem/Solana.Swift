aaad595b7ada68ae3b987f11bde470e08afb15bf
Andrey Chukavin <megadestroyer007@gmail.com>
2024-03-13 21:16:04 +0800

    IOS-6236 Initial code to get serialized transaction messages

--- Sources/Solana/Actions/sendSOL/sendSOL.swift
+++ Sources/Solana/Actions/sendSOL/sendSOL.swift
@@ -1,6 +1,65 @@
 import Foundation
 
 extension Action {
+    public func serializedMessage(
+        to destination: String,
+        amount: UInt64,
+        computeUnitLimit: UInt32?,
+        computeUnitPrice: UInt64?,
+        allowUnfundedRecipient: Bool = false,
+//        signer: Signer,
+        fromPublicKey: PublicKey,
+        onComplete: @escaping ((Result<String, Error>) -> Void)
+    ) {
+//        let fromPublicKey = signer.publicKey
+
+//        if fromPublicKey.base58EncodedString == destination {
+//            onComplete(.failure(SolanaError.other("You can not send tokens to yourself")))
+//            return
+//        }
+
+        // check
+//        if allowUnfundedRecipient {
+//            serializedMessage(from: fromPublicKey, to: destination, amount: amount, computeUnitLimit: computeUnitLimit, computeUnitPrice: computeUnitPrice, onComplete: onComplete)
+//        } else {
+//            self.api.getAccountInfo(account: destination, decodedTo: EmptyInfo.self) { resultInfo in
+//                if case Result.failure( let error) = resultInfo {
+//                    if let solanaError = error as? SolanaError,
+//                       case SolanaError.couldNotRetriveAccountInfo = solanaError {
+//                        // let request through
+//                    } else {
+//                        onComplete(.failure(error))
+//                        return
+//                    }
+//                }
+//                
+//                guard case Result.success(let info) = resultInfo else {
+//                    onComplete(.failure(SolanaError.couldNotRetriveAccountInfo))
+//                    return
+//                }
+//                
+//                guard info.owner == PublicKey.programId.base58EncodedString else {
+//                    onComplete(.failure(SolanaError.other("Invalid account info")))
+//                    return
+//                }
+//                
+//                self.serializedMessage(from: fromPublicKey, to: destination, amount: amount, computeUnitLimit: computeUnitLimit, computeUnitPrice: computeUnitPrice, onComplete: onComplete)
+//            }
+//        }
+//        
+        
+        prep(to: destination, amount: amount, computeUnitLimit: computeUnitLimit, computeUnitPrice: computeUnitPrice, allowUnfundedRecipient: allowUnfundedRecipient, fromPublicKey: fromPublicKey) { result in
+            switch result {
+            case .failure(let error):
+                onComplete(.failure(error))
+            case .success:
+//                self.serializeAndSend(from: fromPublicKey, to: destination, amount: amount, computeUnitLimit: computeUnitLimit, computeUnitPrice: computeUnitPrice, signer: signer, onComplete: onComplete)
+                self.serializedMessage(from: fromPublicKey, to: destination, amount: amount, computeUnitLimit: computeUnitLimit, computeUnitPrice: computeUnitPrice, onComplete: onComplete)
+
+            }
+        }
+    }
+    
     public func sendSOL(
         to destination: String,
         amount: UInt64,
@@ -11,6 +70,68 @@ extension Action {
         onComplete: @escaping ((Result<TransactionID, Error>) -> Void)
     ) {
         let fromPublicKey = signer.publicKey
+//
+//        if fromPublicKey.base58EncodedString == destination {
+//            onComplete(.failure(SolanaError.other("You can not send tokens to yourself")))
+//            return
+//        }
+//
+//        // check
+//        if allowUnfundedRecipient {
+//            serializeAndSend(from: fromPublicKey, to: destination, amount: amount, computeUnitLimit: computeUnitLimit, computeUnitPrice: computeUnitPrice, signer: signer, onComplete: onComplete)
+//        } else {
+//            self.api.getAccountInfo(account: destination, decodedTo: EmptyInfo.self) { resultInfo in
+//                if case Result.failure( let error) = resultInfo {
+//                    if let solanaError = error as? SolanaError,
+//                       case SolanaError.couldNotRetriveAccountInfo = solanaError {
+//                        // let request through
+//                    } else {
+//                        onComplete(.failure(error))
+//                        return
+//                    }
+//                }
+//                
+//                guard case Result.success(let info) = resultInfo else {
+//                    onComplete(.failure(SolanaError.couldNotRetriveAccountInfo))
+//                    return
+//                }
+//                
+//                guard info.owner == PublicKey.programId.base58EncodedString else {
+//                    onComplete(.failure(SolanaError.other("Invalid account info")))
+//                    return
+//                }
+//                
+//                self.serializeAndSend(from: fromPublicKey, to: destination, amount: amount, computeUnitLimit: computeUnitLimit, computeUnitPrice: computeUnitPrice, signer: signer, onComplete: onComplete)
+//            }
+//        }
+        
+        prep(to: destination, amount: amount, computeUnitLimit: computeUnitLimit, computeUnitPrice: computeUnitPrice, allowUnfundedRecipient: allowUnfundedRecipient, fromPublicKey: fromPublicKey) { result in
+            switch result {
+            case .failure(let error):
+                onComplete(.failure(error))
+            case .success:
+                self.serializeAndSend(from: fromPublicKey, to: destination, amount: amount, computeUnitLimit: computeUnitLimit, computeUnitPrice: computeUnitPrice, signer: signer, onComplete: onComplete)
+            }
+        }
+    }
+    
+    // TODO:
+    // TODO:
+    // TODO:
+    // TODO:
+    // TODO:
+    
+    fileprivate func prep(
+        to destination: String,
+        amount: UInt64,
+        computeUnitLimit: UInt32?,
+        computeUnitPrice: UInt64?,
+        allowUnfundedRecipient: Bool = false,
+//        signer: Signer,
+        fromPublicKey: PublicKey,
+        onComplete: @escaping ((Result<Void, Error>) -> Void)
+    ) {
+//        let fromPublicKey = signer.publicKey
 
         if fromPublicKey.base58EncodedString == destination {
             onComplete(.failure(SolanaError.other("You can not send tokens to yourself")))
@@ -19,7 +140,8 @@ extension Action {
 
         // check
         if allowUnfundedRecipient {
-            serializeAndSend(from: fromPublicKey, to: destination, amount: amount, computeUnitLimit: computeUnitLimit, computeUnitPrice: computeUnitPrice, signer: signer, onComplete: onComplete)
+            onComplete(.success(()))
+//            serializeAndSend(from: fromPublicKey, to: destination, amount: amount, computeUnitLimit: computeUnitLimit, computeUnitPrice: computeUnitPrice, signer: signer, onComplete: onComplete)
         } else {
             self.api.getAccountInfo(account: destination, decodedTo: EmptyInfo.self) { resultInfo in
                 if case Result.failure( let error) = resultInfo {
@@ -42,7 +164,43 @@ extension Action {
                     return
                 }
                 
-                self.serializeAndSend(from: fromPublicKey, to: destination, amount: amount, computeUnitLimit: computeUnitLimit, computeUnitPrice: computeUnitPrice, signer: signer, onComplete: onComplete)
+//                self.serializeAndSend(from: fromPublicKey, to: destination, amount: amount, computeUnitLimit: computeUnitLimit, computeUnitPrice: computeUnitPrice, signer: signer, onComplete: onComplete)
+                
+                onComplete(.success(()))
+            }
+        }
+    }
+    
+    fileprivate func serializedMessage(
+        from fromPublicKey: PublicKey,
+        to destination: String,
+        amount: UInt64,
+        computeUnitLimit: UInt32?,
+        computeUnitPrice: UInt64?,
+        onComplete: @escaping ((Result<String, Error>) -> Void)
+    ) {
+        let instructionsResult = sendSolInstructions(from: fromPublicKey, to: destination, amount: amount, computeUnitLimit: computeUnitLimit, computeUnitPrice: computeUnitPrice)
+        
+        let instructions: [TransactionInstruction]
+        switch instructionsResult {
+        case .success(let array):
+            instructions = array
+        case .failure(let error):
+            onComplete(.failure(error))
+            return
+        }
+        
+        self.serializeTransaction(
+            instructions: instructions,
+            signers: [],
+            feePayer: fromPublicKey,
+            mode: .serializeOnly
+        ) {
+            switch $0 {
+            case .success(let transaction):
+                onComplete(.success(transaction))
+            case .failure(let error):
+                onComplete(.failure(error))
             }
         }
     }
@@ -56,9 +214,39 @@ extension Action {
         signer: Signer,
         onComplete: @escaping ((Result<TransactionID, Error>) -> Void)
     ) {
-        guard let to = PublicKey(string: destination) else {
-            onComplete(.failure(SolanaError.invalidPublicKey))
+        let instructionsResult = sendSolInstructions(from: fromPublicKey, to: destination, amount: amount, computeUnitLimit: computeUnitLimit, computeUnitPrice: computeUnitPrice)
+
+        let instructions: [TransactionInstruction]
+        switch instructionsResult {
+        case .success(let array):
+            instructions = array
+        case .failure(let error):
+            onComplete(.failure(error))
             return
+        }
+        
+        self.serializeAndSendWithFee(
+            instructions: instructions,
+            signers: [signer]
+        ) {
+            switch $0 {
+            case .success(let transaction):
+                onComplete(.success(transaction))
+            case .failure(let error):
+                onComplete(.failure(error))
+            }
+        }
+    }
+    
+    fileprivate func sendSolInstructions(
+        from fromPublicKey: PublicKey,
+        to destination: String,
+        amount: UInt64,
+        computeUnitLimit: UInt32?,
+        computeUnitPrice: UInt64?
+    ) -> Result<[TransactionInstruction], Error> {
+        guard let to = PublicKey(string: destination) else {
+            return .failure(SolanaError.invalidPublicKey)
         }
         
         var instructions = [TransactionInstruction]()
@@ -78,17 +266,7 @@ extension Action {
         )
         instructions.append(transferInstruction)
         
-        self.serializeAndSendWithFee(
-            instructions: instructions,
-            signers: [signer]
-        ) {
-            switch $0 {
-            case .success(let transaction):
-                onComplete(.success(transaction))
-            case .failure(let error):
-                onComplete(.failure(error))
-            }
-        }
+        return .success(instructions)
     }
 }
 
