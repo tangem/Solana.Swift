970d0b5ad9ebbe2720dbe7a5c44305fee0470221
Andrey Fedorov <afedorov@tangem.com>
2024-10-19 02:28:50 +0200

    IOS-8290: Rename SPM package to avoid ambiguity
    Signed-off-by: Andrey Fedorov <afedorov@tangem.com>

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Solana",
+    name: "SolanaSwift",
     platforms: [
         .iOS(.v15),
         .macOS(.v12),
@@ -12,9 +12,9 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Solana",
+            name: "SolanaSwift",
             targets: [
-                "Solana",
+                "SolanaSwift",
             ]
         )
     ],
@@ -28,7 +28,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Solana",
+            name: "SolanaSwift",
             dependencies: [
                 .product(name: "TweetNacl", package: "tweetnacl-swiftwrap"),
                 .product(name: "secp256k1", package: "secp256k1.swift"),
